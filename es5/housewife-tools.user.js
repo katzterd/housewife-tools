09f79d333acd780c9a2978080f9cd287794b647b
Juribiyan <smugleaf@ya.ru>
2023-03-03 21:21:21 +0500

    Domain added

--- es5/housewife-tools.user.js
+++ es5/housewife-tools.user.js
@@ -18,12 +18,13 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 // ==UserScript==
 // @name         HouseWife Tools
 // @namespace    https://www.0chan.pl/userjs/
-// @version      1.1.2
+// @version      1.1.3
 // @description  UX extension for 314n.org
 // @updateURL    https://github.com/juribiyan/housewife-tools/raw/master/es5/housewife-tools.meta.js
 // @author       Snivy
 // @include      https://314n.org/*
 // @include      https://314n.ru/*
+// @include      https://314n.0chan.one/*
 // @grant        GM_getResourceText
 // @icon         https://raw.githubusercontent.com/juribiyan/housewife-tools/master/icon.png
 // @resource     baseCSS https://raw.githubusercontent.com/Juribiyan/housewife-tools/master/css/hwt.css
