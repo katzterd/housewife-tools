70866c7f802e0ea1f8b53a0c474b7d6c93caf93c
katzterd <194586003+katzterd@users.noreply.github.com>
2025-04-14 12:16:32 +0300

    Added 314n.1chan.cyou

--- es5/housewife-tools.user.js
+++ es5/housewife-tools.user.js
@@ -18,7 +18,7 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 // ==UserScript==
 // @name         HouseWife Tools
 // @namespace    https://ochan.ru/userjs/
-// @version      1.1.9
+// @version      1.1.10
 // @description  UX extension for 314n.org
 // @updateURL    https://juribiyan.github.io/housewife-tools/es5/housewife-tools.meta.js
 // @downloadURL  https://juribiyan.github.io/housewife-tools/es5/housewife-tools.user.js
@@ -27,6 +27,10 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 // @match        https://314n.ru/*
 // @match        http://314n/*
 // @match        https://314n.0chan.one/*
+// @match        https://314n.1chan.cyou/*
+// @match        http://pinachvoguta4szixhost7yh2xkkemjeerfba6tang7siudwusrlsiqd.onion/*
+// @match        http://pinachv6cgxmcuojuiabrz6fyizzq2m2m4iu25pcmtesx37f3i4q.b32.i2p/*
+// @match        http://[216:acbf:eccd:78f5:f871:7148:dc01:c666]/*
 // @icon         https://juribiyan.github.io/housewife-tools/icon.png
 // ==/UserScript==
 
--- src/housewife-tools.user.js
+++ src/housewife-tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HouseWife Tools
 // @namespace    https://ochan.ru/userjs/
-// @version      1.1.9
+// @version      1.1.10
 // @description  UX extension for 314n.org
 // @updateURL    https://juribiyan.github.io/housewife-tools/src/housewife-tools.meta.js
 // @downloadURL  https://juribiyan.github.io/housewife-tools/src/housewife-tools.user.js
@@ -10,6 +10,10 @@
 // @match        https://314n.ru/*
 // @match        http://314n/*
 // @match        https://314n.0chan.one/*
+// @match        https://314n.1chan.cyou/*
+// @match        http://pinachvoguta4szixhost7yh2xkkemjeerfba6tang7siudwusrlsiqd.onion/*
+// @match        http://pinachv6cgxmcuojuiabrz6fyizzq2m2m4iu25pcmtesx37f3i4q.b32.i2p/*
+// @match        http://[216:acbf:eccd:78f5:f871:7148:dc01:c666]/*
 // @icon         https://juribiyan.github.io/housewife-tools/icon.png
 // ==/UserScript==
 
